5d14cb475d83f101b7d20cc78bcff4056fef21c3
zghibikhira <zghibikhira@gmail.com>
2025-07-05 08:10:54 +0100

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+mongoose.connect('mongodb://localhost:27017/eduplatform', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .catch(err => console.log(err));
